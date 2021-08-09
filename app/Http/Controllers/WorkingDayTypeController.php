a70901b2c49d7511e7cdb947985438a25a1db706
Julian Murphy <njmdistrisoft@gmail.com>
2021-08-09 01:09:05 -0300

    get working day type from api

--- app/Http/Controllers/WorkingDayTypeController.php
+++ app/Http/Controllers/WorkingDayTypeController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\WorkingDayType;
+use App\Traits\CreateTrait;
+use App\Traits\ListTrait;
+use App\Traits\UpdateTrait;
+use App\Traits\ViewTrait;
 use Illuminate\Http\Request;
 use App\Traits\CrudTrait;
 
 class WorkingDayTypeController extends Controller
 {
-    use CrudTrait;
+    use ListTrait;
+    use CreateTrait;
+    use ViewTrait;
+    use UpdateTrait;
 
     public function setup()
     {
         $this->setModel(WorkingDayType::class);
         $this->setNomenclature('tipo de jornada', 'tipos de jornada');
-        //$this->setListRequest()
     }
 
 }
